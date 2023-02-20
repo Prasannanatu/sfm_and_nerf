65ee4703f4ea881e6cf6ddc09d2f24db6afd7b38
Prasanna Natu <natu.prasanna02@gmail.com>
2023-02-20 17:13:56 -0500

    just updated camera pose but not finalised

--- sfm_p/Phase1/EstimateCameraPose.py
+++ sfm_p/Phase1/EstimateCameraPose.py
@@ -10,12 +10,17 @@ def estimate_camera_pose(K, E):
     Input: Essential matrix 
     getting the value of U, V_T from the Essential Matrix.
     
+
+
+
     Output: 4 possible Camera Poses.
 
     
 
     """
-
+    R= []
+    P =[]
+    T =[]
     U,D,V_T = np.linalg.svd(E)
 
     W = np.array[[0, -1, 0],
@@ -27,19 +32,39 @@ def estimate_camera_pose(K, E):
     
 
     R_1 = U@W@V_T
+    R.append(R_1)
     R_2 = U@(np.transpose(W))@V_T
-
+    R.append(R_2)
     ones = np.array([[1, 0, 0],
                     [0, 1, 0],
                     [0, 0, 1]])
     Translation_1 = np.hstack(ones ,-C1)
     Translation_2 = np.hstack(ones, -C2)
 
-    P_1 =  K @ R_1 @ Translation_1
-    P_2 =  K @ R_1 @ Translation_2
-    P_3 =  K @ R_2 @ Translation_1
-    P_4 =  K @ R_2 @ Translation_2
+    T.append(Translation_1)
+    T.append(Translation_2)
 
-    return P_1, P_2, P_3, P_4
+    for i in range(len(R)):
+        for j in range(len(T)):
+            if (np.linalg.det(R[i] < 0)):
+                R[i] = -R[i]
+                T[i] = -T[i]
+            # P.append(K@R@T)
+
+
+
+
+
+    # P_1 =  K @ R_1  @ Translation_1
+    # P_2 =  K @ R_1  @ Translation_2
+    # P_3 =  K @ R_2  @ Translation_1
+    # P_4 =  K @ R_2  @ Translation_2
+
+    # return P_1, P_2, P_3, P_4
+
+
+
+
+    return R, T
 
 
